fe89f3572a576476c977d5d98d098fe81b22772b
sakib <s.sarder232002@gmail.com>
2023-03-06 12:24:27 +0600

    money master is fixed properly

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const income = document.querySelector("#income");
+const calculateExpense = () => {
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes =document.querySelectorAll("#clothes")[0].value;
+
+  if (
+    income.value < 0 ||
+    income.value == "" ||
+    food < 0 ||
+    food == "" ||
+    rent < 0 ||
+    rent == "" ||
+    clothes < 0 ||
+    clothes == ""
+  ) {
+    alert("Inputs must be positive numbers");
+    return;
+  }
+  // calculate expense
+  const expense =
+    parseInt(food) + parseInt(rent) + parseInt(clothes);
+
+  // calculate balance
+  const balance = parseInt(income.value) - expense;
+  //   validate income
+  if (expense > income.value) {
+    alert("Expenses cannot be more than income");
+  } else {
+    // view total expense and balance
+    document.getElementById("total-expense").innerText = expense;
+    document.getElementById("balance").innerText = balance;
+  }
+};
+
+const calculateSavings = () => {
+  // calculate saving amount
+  const savePercentage = document.getElementById("save").value;
+//   Validate saving percentage value
+  if (savePercentage < 0) {
+    alert("Provide positive saving value");
+    return;
+  }
+  const savingAmount = (savePercentage / 100) * income.value;
+
+  // calculate remaining balance
+  const balance = document.getElementById("balance").innerText;
+  const remainingBalance = balance - savingAmount;
+
+  //   validate saving amount
+  if (savingAmount > balance) {
+    alert("SavingAmount is greater than balance");
+  } else {
+    // view saving amount and remaining balance
+    document.getElementById("saving-amount").innerText = savingAmount.toFixed(2);
+    document.getElementById("remaining-balance").innerText = remainingBalance;
+  }
+};
